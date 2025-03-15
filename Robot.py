b0cd4dbed93edb92bdb1fc5c7904ed41c47a5ded
zmeyer1 <zane.meyer@gmail.com>
2025-03-14 17:51:01 -0700

    bugfix

--- Robot.py
+++ Robot.py
@@ -40,12 +40,12 @@ class Robot():
             data = data.strip('"')
             # save the gesture if it is larger than 0
             if(len(data) > 0):
-                if data == "die":
-                    break
                 self.latest_gesture = data
                 print(f"New Gesture: {self.latest_gesture}")
             # close the connection
             connection.close()
+            if data == "die":
+                break
 
 
     def execute_latest_gesture(self):
